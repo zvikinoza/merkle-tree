6df1a523619b4bddbb62229fca10611d725e6260
zvikinoza <znoza17@freeuni.edu.ge>
2020-09-25 02:31:25 +0400

    it's late, only func declarations listed

--- merkletree/merkletree.go
+++ merkletree/merkletree.go
@@ -1,2 +1,56 @@
 package merkletree
 
+import (
+	"crypto/sha256"
+	"hash"
+)
+
+// Data to be added in merkle tree must
+// have comarison and calculating hash functions
+type Data interface {
+	Equals(other Data) (bool, error)
+	Hash() ([]byte, error)
+}
+
+// MerkleTree ...
+type MerkleTree struct {
+	root   *node
+	data   []Data
+	hashfn func() hash.Hash
+}
+
+type node struct {
+	left  *node
+	right *node
+	hash  hash.Hash
+}
+
+// NewMerkleTree ...
+func NewMerkleTree(d []Data) (*MerkleTree, error) {
+	return NewCostumHasMerkleTree(d, sha256.New)
+}
+
+// NewCostumHasMerkleTree ...
+func NewCostumHasMerkleTree(d []Data, hash func() hash.Hash) (*MerkleTree, error) {
+	mt := MerkleTree{
+		root:   nil,
+		data:   d,
+		hashfn: hash,
+	}
+
+	return &mt, nil
+}
+
+// GetRootHash ...
+func (mt *MerkleTree) GetRootHash() []byte {
+	return mt.root.hash.Sum(nil)
+}
+
+// Validate entire trees' correctness
+func (mt *MerkleTree) Validate() (bool, error) {
+	return false, nil
+}
+
+func (mt *MerkleTree) String() (string, error) {
+	return "", nil
+}
